887cf810462c9e72c00b797d6aaed576fac82247
Julio Felix <juliofelix848@gmail.com>
2022-09-08 19:12:52 -0300

    add Scanner on Main for payload

--- redes-vp1/src/main/Main.java
+++ redes-vp1/src/main/Main.java
@@ -1,11 +1,17 @@
 package main;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		String payload = "";
-
+		int i = 0;
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Digite um Payload:");
+	    payload = sc.nextLine();
+	    //System.out.println();
+	    sc.close(); //Encerra o programa
 	}
 
 }
